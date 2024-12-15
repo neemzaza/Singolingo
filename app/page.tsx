305391f2a3752106f4c29442e3e857140d37ae3f
Airwavy <airwavy@MacBook-Pro-khxng-Airwavy.local>
2024-12-16 06:33:29 +0700

    lll

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,12 @@
 
 import Image from "next/image";
 import { translate } from "google-translate-api-browser"
-import { SetStateAction, useEffect, useState } from "react";
+import { SetStateAction, useEffect, useState, useRef } from "react";
+
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 import { generate, count } from "random-words";
+import Head from "next/head";
 
 
 export default function Home() {
@@ -19,11 +22,20 @@ export default function Home() {
   const [answer, setAnswer] = useState<string>("")
   const [choice, setChoice] = useState<string[]>([])
 
+  const [answerKey, setAnswerKey] = useState<number>(-1);
+  const [answerVal, setAnswerVal] = useState<string>("");
+
   const [score, setScore] = useState(0);
   const [mistake, setMistake] = useState(0);
 
+  const [startState, setStartState] = useState(false);
+
+  const [error, setError] = useState(false);
+
+  const bestScore = useRef(0);
+
   const vocabRandom = async () => {
-    let randWord: any = generate()
+    let randWord: any = generate({wordsPerString: 2})
 
     setWord(randWord)
 
@@ -34,6 +46,7 @@ export default function Home() {
         // console.info("Answer OK")
       }).catch((err: any) => {
         console.error(err);
+        setError(true);
       })
   }
 
@@ -63,6 +76,7 @@ export default function Home() {
 
         }).catch((err: any) => {
           console.error(err);
+          setError(true);
         })
     }
 
@@ -74,16 +88,36 @@ export default function Home() {
 
   // Answering
   const answerQuestion = (word: string) => {
+    if (answerKey === -1) {
+      alert("Please select your answer");
+    }
+
+    setAnswerVal("");
+    setAnswerKey(-1);
     if (word === answer) {
       setScore(score + 1);
+      
+      if (score >= bestScore.current) {
+        bestScore.current = bestScore.current + 1;
+      }
+
     } else {
       setMistake(mistake + 1);
+      setScore(0);
     }
 
     findingWord();
   }
 
+  const selectAnswer = (key: number, val: string) => {
+    setAnswerKey(key);
+    setAnswerVal(val);
+  }
+
   const findingWord = async () => {
+    setStartState(true);
+
+    // if (!randomState) {
 
     // Reset random
     setChoice([]);
@@ -105,6 +139,7 @@ export default function Home() {
         console.info("Answer OK")
       }).catch((err: any) => {
         console.error(err);
+        setError(true);
       })
 
     // Random other choice
@@ -134,11 +169,12 @@ export default function Home() {
 
         }).catch((err: any) => {
           console.error(err);
+          setError(true);
         })
     }
 
     setLoading(false)
-
+  // }
 
   }
 
@@ -176,14 +212,26 @@ export default function Home() {
       }
     }
   }, [answer, choice])
+
+  interface sendAnswerWow {
+    key: number,
+    val: string
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <>
+      <Head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
+      </Head>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Score&nbsp;
           <code className="font-mono font-bold">{score}</code> &nbsp;| 
           Mistake&nbsp;
-          <code className="font-mono font-bold">{mistake}</code>
+          <code className="font-mono font-bold text-red-400">{mistake}</code> &nbsp;| 
+          Best Score&nbsp;
+          <code className="font-mono font-bold text-lime-400">{bestScore.current}</code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
           <a
@@ -206,18 +254,23 @@ export default function Home() {
         {/* <p className="text-center">{answer}</p> */}
       </div>
 
+      {
+          (!startState) ? (
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="#"
-          onClick={() => findingWord()}
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors bg-blue-800 hover:border-gray-300 hover:bg-blue-100 hover:dark:border-neutral-700 hover:dark:bg-blue-800/30"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Start
+        
+          <a
+            href="#"
+            onClick={() => findingWord()}
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors bg-blue-800 hover:border-gray-300 hover:bg-blue-100 hover:dark:border-neutral-700 hover:dark:bg-blue-800/30"
+            rel="noopener noreferrer"
+          >
+            <h2 className="mb-3 text-2xl font-semibold text-center flex ">
+            <i className="bi bi-caret-right"></i>
+              Start
 
-          </h2>
-        </a>
+            </h2>
+          </a>
+          
         <br />
         {/* <hr /> */}
         <br />
@@ -254,6 +307,10 @@ export default function Home() {
 
 
       </div>
+      ) :
+      <></>
+
+    }
 
       <div className="choice mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         { (!loading) ? choice.map((val, key) => (
@@ -261,22 +318,35 @@ export default function Home() {
             <a
               href="#"
               key={key}
-              onClick={() => answerQuestion(val)}
-              className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+              // onClick={() => answerQuestion(val)}
+              onClick={() => selectAnswer(key, val)}
+              className={((key == answerKey) ? "bg-blue-800 border-blue-400 hover:bg-blue-500 hover:dark:border-neutral-700" : "hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30") + " group rounded-lg border border-transparent px-5 py-4 transition-colors "}
               rel="noopener noreferrer"
             >
               <h2 className="mb-3 text-2xl font-semibold">
-                {val}{" "}
+              <i className="bi bi-arrow-right-short"></i>
                 <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                  -&gt;
+                  {val}
                 </span>
               </h2>
             </a>
             <br />
+            
           </>
         )) : <>Loading...</>}
+        {(error) ? <>
+          <h1 className="text-red-600 text-5xl">
+            <b>An Error Happen!</b>
+          </h1>
+        </> : <></>}
+       {
+        (startState && !loading) ? <> <a onClick={() => answerQuestion(answerVal)} className="rounded-lg cursor-pointer font-bold text-center bg-blue-600 border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-blue-800/30">
+          <i className="bi bi-send-fill"></i>&nbsp;
+          Send Answer</a></> : <></>
+       }
       </div>
 
     </main>
+    </>
   );
 }
